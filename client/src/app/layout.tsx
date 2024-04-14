603e9cf96634158ab20231d9359f0fd2516aac00
Ashish Kumar Mohanty <ashishkumarmohanty7797@gmail.com>
2024-04-14 20:33:28 +0530

    fix z-index for mobile nav

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                   <Sidebar />
                 </div>
                 <div className='w-[100%] md:w-[77%] px-[2%] md:px-[4%] lg:px-[6%] xl:px-[8%] py-6 overflow-auto'>
-                  <div className="mb-[4%] flex md:hidden fixed top-0 left-0 w-full shadow-md py-2 z-50"><DrawerSidebar/></div>
+                  <div className="mb-[4%] flex md:hidden fixed top-0 left-0 w-full shadow-md py-2 z-50 bg-white"><DrawerSidebar/></div>
                   <div className="mt-[40px] md:mt-0">
                     {children}
                   </div>
