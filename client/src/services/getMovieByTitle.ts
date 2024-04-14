d817b7d34e2a8ef22e8f8a963f2318e95c4caca0
Ashish Kumar Mohanty <ashishkumarmohanty7797@gmail.com>
2024-04-14 18:04:49 +0530

    Update getMovieByTitle.ts

--- client/src/services/getMovieByTitle.ts
+++ client/src/services/getMovieByTitle.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-export async function getMoviewByTitle(movieTitle:string){
-    console.log(movieTitle)
-    try {
-        const response:any = await axios.get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=5a31aba3`)
-        return response.data;
-    } catch (error) {
-        return error;
-    }
+export async function getMoviewByTitle(movieTitle: string) {
+  console.log(movieTitle);
+  try {
+    const response: any = await axios.get(
+      `https://www.omdbapi.com/?t=${movieTitle}&apikey=5a31aba3`
+    );
+    return response.data;
+  } catch (error) {
+    return error;
+  }
 }
